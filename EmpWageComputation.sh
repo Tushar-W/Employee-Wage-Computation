1f4915e8ee410bc43315b0c7be293a5d0b9d417b
Tushar-W <twankhede95@gmail.com>
2020-03-30 00:19:16 +0530

    ADD: calculate wages for a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,18 +6,25 @@ echo	"Welcome To Employee Wage Computation"
 IS_FULL_TIME=1;
 IS_PART_TIME=2;
 EMP_WAGE_PER_HR=20;
+WORKING_DAYS_PER_MONTH=20;
 #VARIABLES
-randomTime=$((RANDOM%3));
+monthlyWage=0;
 
-case	$randomTime	in
-	$IS_FULL_TIME)
-			workHr=8
-			;;
-	$IS_PART_TIME)
-			workHr=4
-			;;
-	*)
-			workHr=0
-			;;
-esac
-echo	wage=$(($workHr*$EMP_WAGE_PER_HR));
+for	((	day=1;	day<=$WORKING_DAYS_PER_MONTH;	day++	))
+do
+	randomTime=$((RANDOM%3));
+	case	$randomTime	in
+		$IS_FULL_TIME)
+				workHr=8
+				;;
+		$IS_PART_TIME)
+				workHr=4
+				;;
+		*)
+				workHr=0
+				;;
+	esac
+	wage=$(($workHr*$EMP_WAGE_PER_HR));
+	monthlyWage=$(($monthlyWage+$wage));
+done
+echo	"Wages for a Month : "$monthlyWage
