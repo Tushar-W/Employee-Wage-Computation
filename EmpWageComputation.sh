12ff66d353697cb3f0bf53c3dfe41829ba42e511
Tushar-W <twankhede95@gmail.com>
2020-03-29 23:32:13 +0530

    ADD: calculate wage using case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -5,18 +5,19 @@ echo	"Welcome To Employee Wage Computation"
 #CONSTANTS
 IS_FULL_TIME=1;
 IS_PART_TIME=2;
+EMP_WAGE_PER_HR=20;
 #VARIABLES
-empWagePerHr=20;
-randomNum=$((RANDOM%3));
+randomTime=$((RANDOM%3));
 
-if	[	$IS_FULL_TIME	-eq	$randomNum	];
-then
-	workHr=8;
-elif	[	$IS_PART_TIME	-eq	$randomNum	];
-then
-	workHr=4;
-else
-	workHr=0;
-fi
-
-echo	wage=$(($workHr*$empWagePerHr));
+case	$randomTime	in
+	$IS_FULL_TIME)
+			workHr=8
+			;;
+	$IS_PART_TIME)
+			workHr=4
+			;;
+	*)
+			workHr=0
+			;;
+esac
+echo	wage=$(($workHr*$EMP_WAGE_PER_HR));
