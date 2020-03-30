ecfd213dae748b11421e3432ea1ce098597cafab
Tushar-W <twankhede95@gmail.com>
2020-03-30 09:01:40 +0530

    ADD:calculate wages till days reached

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -7,24 +7,28 @@ IS_FULL_TIME=1;
 IS_PART_TIME=2;
 EMP_WAGE_PER_HR=20;
 WORKING_DAYS_PER_MONTH=20;
+HRS_IN_MONTH=100;
 #VARIABLES
-monthlyWage=0;
+totalWorkHr=0;
+workingDays=0;
 
-for	((	day=1;	day<=$WORKING_DAYS_PER_MONTH;	day++	))
+while	[[	$totalWorkHr	-lt	$HRS_IN_MONTH	]]	&&	[[	$workingDays	-lt	$WORKING_DAYS_PER_MONTH	]]
 do
 	randomTime=$((RANDOM%3));
 	case	$randomTime	in
 		$IS_FULL_TIME)
-				workHr=8
-				;;
+			workHr=8
+			;;
 		$IS_PART_TIME)
-				workHr=4
-				;;
+			workHr=4
+			;;
 		*)
-				workHr=0
-				;;
+			workHr=0
+			;;
 	esac
-	wage=$(($workHr*$EMP_WAGE_PER_HR));
-	monthlyWage=$(($monthlyWage+$wage));
+	((workingDays++));
+	totalWorkHr=$((totalWorkHr	+	workHr));
 done
-echo	"Wages for a Month : "$monthlyWage
+echo	monthlyWage=$((totalWorkHr	*	EMP_WAGE_PER_HR));
+
+
