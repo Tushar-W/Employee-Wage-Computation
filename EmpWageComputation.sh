16f9842686f35dc74c89c168e897acf0e0ec22b7
Tushar-W <twankhede95@gmail.com>
2020-04-01 10:21:49 +0530

    ADDED:features for calculating employee daily wages

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,6 +1,6 @@
-#!/bin/bash	-x
+#!/bin/bash -x
 
-echo	"Welcome To Employee Wage Computation"
+echo "Welcome To Employee Wage Computation"
 
 #CONSTANTS
 IS_PRESENT=1;
@@ -11,9 +11,9 @@ fullDayHr=8;
 wage=0;
 randomNum=$((RANDOM%2));
 
-if	[	$IS_PRESENT	-eq	$randomNum	];
+if [ $IS_PRESENT -eq $randomNum ];
 then
-	echo wage=$((empWagePerHr*fullDayHr));
+		echo wage=$((empWagePerHr*fullDayHr));
 else
-	echo "wage= "$wage
+		echo "wage=" $wage
 fi
