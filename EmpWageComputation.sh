b6c28a86fb41a86a7ff38aefdedeedda5425e97e
Tushar-W <twankhede95@gmail.com>
2020-04-01 16:26:40 +0530

    REFACTORED:check employee wage using case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 echo "Welcome To Employee Wage Computation"
-
 #CONSTANTS
 IS_FULL_TIME=1;
 IS_PART_TIME=2;
@@ -10,14 +9,14 @@ EMP_WAGE_PER_HR=20;
 randomTime=$((RANDOM%3));
 
 case $randomTime in
-				$IS_FULL_TIME)
-									workHr=8
-									;;
-				$IS_PART_TIME)
-									workHr=4
-									;;
-				*)
-									workHr=0
-									;;
+	$IS_FULL_TIME)
+			workHr=8
+			;;
+	$IS_PART_TIME)
+			workHr=4
+			;;
+	*)
+			workHr=0
+			;;
 esac
 echo wage=$(($workHr*$EMP_WAGE_PER_HR));
