41d39dcf6e26e28dc503b5cd1b8231a49ab91ae3
Tushar-W <twankhede95@gmail.com>
2020-04-01 16:11:32 +0530

    MODIFIED: changes in indentation

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 echo "Welcome To Employee Wage Computation"
-
 #CONSTANTS
 IS_FULL_TIME=1;
 IS_PART_TIME=2;
@@ -11,12 +10,12 @@ randomNum=$((RANDOM%3));
 
 if [ $IS_FULL_TIME -eq $randomNum ];
 then
-		workHr=8;
+	workHr=8;
 elif [ $IS_PART_TIME -eq $randomNum ];
 then
-		workHr=4;
+	workHr=4;
 else
-		workHr=0;
+	workHr=0;
 fi
 
 echo wage=$(($workHr*$empWagePerHr));
