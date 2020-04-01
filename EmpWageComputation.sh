823a9d4328d9507315ee3fc032b2e4872bb3f365
Tushar-W <twankhede95@gmail.com>
2020-04-01 10:55:51 +0530

    REFACTORED:fuctions for getting employee work hour

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,6 +1,6 @@
-#!/bin/bash	-x
+#!/bin/bash -x
 
-echo	"Welcome To Employee Wage Computation"
+echo "Welcome To Employee Wage Computation"
 
 #CONSTANTS
 IS_FULL_TIME=1;
@@ -12,26 +12,26 @@ HRS_IN_MONTH=100;
 totalWorkHr=0;
 workingDays=0;
 
-function	getWorkHours()	{
-	case	$1	in
-		$IS_FULL_TIME)
-			workHr=8
-			;;
-		$IS_PART_TIME)
-			workHr=4
-			;;
-		*)
-			workHr=0
-			;;
-	esac
-	echo	$workHr
+function getWorkHours() {
+		case $1 in
+					$IS_FULL_TIME)
+										workHr=8
+										;;
+					$IS_PART_TIME)
+										workHr=4
+										;;
+					*)
+										workHr=0
+										;;
+		esac
+		echo $workHr
 }
 
-while	[[	$totalWorkHr	-lt	$HRS_IN_MONTH	]]	&&	[[	$workingDays	-lt	$WORKING_DAYS_PER_MONTH	]]
+while [[$totalWorkHr -lt $HRS_IN_MONTH]] && [[$workingDays -lt $WORKING_DAYS_PER_MONTH]]
 do
-	randomTime=$((RANDOM%3));
-	workHr="$(	getWorkHours	$randomTime	)"
-	((workingDays++));
-	totalWorkHr=$((totalWorkHr	+	workHr));
+		randomTime=$((RANDOM%3));
+		workHr="$( getWorkHours $randomTime )"
+		((workingDays++));
+		totalWorkHr=$((totalWorkHr+workHr));
 done
-echo	monthlyWage=$((totalWorkHr	*	EMP_WAGE_PER_HR));
+echo monthlyWage=$((totalWorkHr*EMP_WAGE_PER_HR));
