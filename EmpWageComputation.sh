562b188a470f9522913b21db8e9571233ee605fd
Tushar-W <twankhede95@gmail.com>
2020-04-01 11:28:00 +0530

    ADDED:features for storing days and daily wages in dictionary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,7 +1,7 @@
-#!/bin/bash	-x
+#!/bin/bash -x
 
-echo	"Welcome To Employee Wage Computation"
-declare	-A	empWagePerDay
+echo "Welcome To Employee Wage Computation"
+declare -A empWagePerDay
 #CONSTANTS
 IS_FULL_TIME=1;
 IS_PART_TIME=2;
@@ -12,34 +12,34 @@ HRS_IN_MONTH=100;
 totalWorkHr=0;
 workingDays=0;
 
-function	getWorkHours()	{
-	case	$1	in
-		$IS_FULL_TIME)
-			workHr=8
-			;;
-		$IS_PART_TIME)
-			workHr=4
-			;;
-		*)
-			workHr=0
-			;;
-	esac
-	echo	$workHr
+function getWorkHours() {
+				case $1 in
+						$IS_FULL_TIME)
+											workHr=8
+											;;
+						$IS_PART_TIME)
+											workHr=4
+											;;
+						*)
+											workHr=0
+											;;
+				esac
+				echo $workHr
 }
-function	getDailyWage()	{
-	local workHrPerDay=$1;
-	wagePerDay=$((workHrPerDay	*	EMP_WAGE_PER_HR));
-	echo	$wagePerDay
+function getDailyWage() {
+				local workHrPerDay=$1;
+				wagePerDay=$((workHrPerDay*EMP_WAGE_PER_HR));
+				echo $wagePerDay
 }
 
-while	[[	$totalWorkHr	-lt	$HRS_IN_MONTH	]]	&&	[[	$workingDays	-lt	$WORKING_DAYS_PER_MONTH	]]
+while [[$totalWorkHr -lt $HRS_IN_MONTH]] && [[$workingDays -lt $WORKING_DAYS_PER_MONTH]]
 do
-	randomTime=$((RANDOM%3));
-	((workingDays++));
-	workHr="$(	getWorkHours	$randomTime	)"
-	totalWorkHr=$((totalWorkHr	+	workHr));
-	empWagePerDay[$workingDays]="$(	getDailyWage	$workHr	)"
+		randomTime=$((RANDOM%3));
+		((workingDays++));
+		workHr="$( getWorkHours $randomTime )"
+		totalWorkHr=$((totalWorkHr+workHr));
+ 		empWagePerDay[$workingDays]="$( getDailyWage $workHr )"
 done
-echo	monthlyWage=$((totalWorkHr	*	EMP_WAGE_PER_HR));
-echo	"Daily Wage : "${empWagePerDay[@]}
-echo	"Day : "${!empWagePerDay[@]}
+echo monthlyWage=$((totalWorkHr*EMP_WAGE_PER_HR));
+echo "Daily Wage:" ${empWagePerDay[@]}
+echo "Day:" ${!empWagePerDay[@]}
