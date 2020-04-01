25132ebd5b2c4825ca0db95751b7cb01912eac27
Tushar-W <twankhede95@gmail.com>
2020-04-01 17:22:17 +0530

    MODIFIED:added fuction to get monthly wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 echo "Welcome To Employee Wage Computation"
-
 #CONSTANTS
 IS_FULL_TIME=1;
 IS_PART_TIME=2;
@@ -10,21 +9,25 @@ WORKING_DAYS_PER_MONTH=20;
 #VARIABLES
 monthlyWage=0;
 
-for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
-do
-		randomTime=$((RANDOM%3));
-		case $randomTime in
-							$IS_FULL_TIME)
-												workHr=8
-												;;
-							$IS_PART_TIME)
-												workHr=4
-												;;
-							*)
-												workHr=0
-												;;
-		esac
-		wage=$(($workHr*$EMP_WAGE_PER_HR));
-		monthlyWage=$(($monthlyWage+$wage));
-done
+function getMonthlyWage() {
+		for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
+		do
+			randomTime=$((RANDOM%3));
+			case $randomTime in
+					$IS_FULL_TIME)
+						workHr=8
+						;;
+					$IS_PART_TIME)
+						workHr=4
+						;;
+					*)
+						workHr=0
+						;;
+			esac
+			wage=$(($workHr*$EMP_WAGE_PER_HR));
+			monthlyWage=$(($monthlyWage+$wage));
+		done
+		echo $monthlyWage
+}
+monthlyWage=$( getMonthlyWage )
 echo "Wages for a Month:" $monthlyWage
