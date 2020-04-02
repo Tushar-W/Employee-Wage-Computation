de351b801eb01a23f9312775e9cce3264b73e3e7
Tushar-W <twankhede95@gmail.com>
2020-04-02 14:50:48 +0530

    REFACTORED:function for storing employee daily wages and days

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -13,33 +13,35 @@ totalWorkHr=0;
 workingDays=0;
 
 function getWorkHours() {
-				case $1 in
-						$IS_FULL_TIME)
-											workHr=8
-											;;
-						$IS_PART_TIME)
-											workHr=4
-											;;
-						*)
-											workHr=0
-											;;
-				esac
-				echo $workHr
+	case $1 in
+		$IS_FULL_TIME)
+			workHr=8
+			;;
+		$IS_PART_TIME)
+			workHr=4
+			;;
+		*)
+			workHr=0
+			;;
+	esac
+	echo $workHr
 }
 function getDailyWage() {
-				local workHrPerDay=$1;
-				wagePerDay=$((workHrPerDay*EMP_WAGE_PER_HR));
-				echo $wagePerDay
+	local workHrPerDay=$1;
+	wagePerDay=$((workHrPerDay * EMP_WAGE_PER_HR));
+	echo $wagePerDay
+}
+function calculateTotalWorkHr() {
+	while [[ $totalWorkHr -lt $HRS_IN_MONTH && $workingDays -lt $WORKING_DAYS_PER_MONTH ]]
+	do
+		randomTime=$((RANDOM%3))
+		workHr=$( getWorkHours $randomTime )
+ 		empWagePerDay[$workingDays]=$( getDailyWage $workHr )
+		totalWorkHr=$((totalWorkHr + workHr))
+		((workingDays++));
+	done
+	echo ${empWagePerDay[@]}
 }
 
-while [[$totalWorkHr -lt $HRS_IN_MONTH]] && [[$workingDays -lt $WORKING_DAYS_PER_MONTH]]
-do
-		randomTime=$((RANDOM%3));
-		((workingDays++));
-		workHr="$( getWorkHours $randomTime )"
-		totalWorkHr=$((totalWorkHr+workHr));
- 		empWagePerDay[$workingDays]="$( getDailyWage $workHr )"
-done
-echo monthlyWage=$((totalWorkHr*EMP_WAGE_PER_HR));
+empWagePerDay=$( calculateTotalWorkHr )
 echo "Daily Wage:" ${empWagePerDay[@]}
-echo "Day:" ${!empWagePerDay[@]}
